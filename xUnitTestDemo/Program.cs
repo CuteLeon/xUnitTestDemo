9861d3c9d03d1784c8ac5bb6079a91c03c4ba12c
Leon <Leon.Engineer@hotmail.com>
2020-06-09 22:01:19 +0800

    Add Calculator class;

---
+++ xUnitTestDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xUnitTestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
