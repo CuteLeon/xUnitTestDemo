eadffd3e59c65b330adf4971d751ff038f95bff9
Leon <Leon.Engineer@hotmail.com>
2020-06-09 23:27:43 +0800

    Attribute

--- xUnitTestDemoTests/CalculatorTests.cs
+++ xUnitTestDemoTests/CalculatorTests.cs
@@ -2,9 +2,15 @@
 
 namespace xUnitTestDemo.Tests
 {
+    [Trait("测试目录-Class", "单元测试-Class")]
     public class CalculatorTests
     {
+        /// <summary>
+        /// 使用 [Fact] 标记方法为单元测试方法，可以使用 Skip 和 Timeout 参数设置跳过或超时
+        /// 使用 [Trait] 标记单元测试的分组特征，在 测试资源管理器窗口>分组依据 勾选特征
+        /// </summary>
         [Fact()]
+        [Trait("测试目录-Method", "单元测试-Method")]
         public void AddTest()
         {
             // Arrange
